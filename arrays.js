81ba24c9312aa0b182fee165cff17d3e6fe9ad0c
Michael Vasios <mvasios1@gmail.com>
2017-12-02 18:11:28 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,32 @@
+chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  array.shift(element)
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.shift(element)
+}
+
+function addElementToEndOfArray(array, element) {
+  array.push(element)
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index) {
+  array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.splice(0)
+}
+
+function removeElementFromBeginningOfArray(array) {
+  [...array.slice(1)]
+}
+
