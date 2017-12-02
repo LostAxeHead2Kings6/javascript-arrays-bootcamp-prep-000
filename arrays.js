aff943084de9aa4cf510aa1a68708ee4237924be
Michael Vasios <mvasios1@gmail.com>
2017-12-02 18:23:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,6 +7,7 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array = [element, ...array]
+  return array
 }
 
 function addElementToEndOfArray(array, element) {
